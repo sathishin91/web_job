153e0e887fc74ebc35b09808f3b098528fcc6b08
vivekaviva <vivekaloganathan37@gmail.com>
2023-11-30 12:57:29 +0530

    With the job detail page

--- src/app/job/job-detail/job-detail.component.ts
+++ src/app/job/job-detail/job-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-job-detail',
@@ -9,4 +10,10 @@ export class JobDetailComponent {
   page = 4;
   page2 = 1;
   currentPage = 3;
+
+  constructor(private modalService: NgbModal) {}
+
+  openVerticallyCentered(content: any) {
+    this.modalService.open(content, { centered: true });
+  }
 }
