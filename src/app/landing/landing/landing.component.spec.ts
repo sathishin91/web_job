369e0bb5cbb12b87e73a16002ba77116ca5d0356
Dharun <rubanselvamcs@gamil.com>
2023-12-07 16:27:33 +0530

    Ui Changes

--- src/app/landing/landing/landing.component.spec.ts
+++ src/app/landing/landing/landing.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
 
+
+
 describe('LandingComponent', () => {
   let component: LandingComponent;
   let fixture: ComponentFixture<LandingComponent>;
